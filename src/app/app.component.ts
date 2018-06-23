0df3365f0a2ede98fc26e37cee3caaeca55b1b9f
Calvin Urankar <curankar@localhost.localdomain>
2018-06-23 18:15:06 +0200

    Did Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-java';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  linestart = '<span class="line">';
+  lineend = '</span>';
+  public text: string;
+
+  public highlight() {
+      const string = $('#text').val();
+      this.text = Prism.highlight(string, Prism.languages.java, 'java');
+      const lines: string[] = this.text.match(/^.*([\n\r]+|$)/gm);
+      const lones: string[] = new Array(lines.length);
+      lines.forEach((line, index, arr) => {
+          lones[index] = this.linestart + line.trim() + this.lineend;
+      });
+      console.log(lones);
+      this.text = lones.join('\n\r');
+  }
 }
